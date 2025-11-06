fbaf82284eaf9ff38e854d42234eefcc4f94d6ae
radeflex <maks.krav61@gmail.com>
2025-11-06 11:12:02 +0300

    first commit

---
+++ src/main/java/by/radeflex/steamshop/SteamShopApplication.java
@@ -0,0 +1,11 @@
+package by.radeflex.steamshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SteamShopApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SteamShopApplication.class, args);
+    }
+}
