2ee00ce3545260fe5d00c14ad86bc43727a4e47a
radeflex <maks.krav61@gmail.com>
2025-11-07 23:57:05 +0300

    minor changes

--- src/main/java/by/radeflex/steamshop/dto/ProductCreateEditDto.java
+++ src/main/java/by/radeflex/steamshop/dto/ProductCreateEditDto.java
@@ -1,21 +1,22 @@
 package by.radeflex.steamshop.dto;
 
 import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import org.hibernate.validator.constraints.URL;
 
 public record ProductCreateEditDto(
-        @NotNull(message="не может быть пустым")
+        @NotBlank(message="не может быть пустым")
         @Size(min=10, max=32, message="от 10 до 32 символов")
         String title,
-        @NotNull(message="не может быть пустым")
+        @NotBlank(message="не может быть пустым")
         @Size(max=500, message="не более 500 символов")
         String description,
         @NotNull
         @Min(value=1, message="минимум 1₽")
         Integer price,
-        @NotNull(message="укажите превью")
+        @NotBlank(message="не может быть пустым")
         @URL(message="должен быть формата URL")
         String previewUrl
 ) { }
