c7f73974fd8983278f71b9b7df47f95d600537ae
Radeflex <maks.krav61@gmail.com>
2026-01-27 18:49:10 +0300

    added cors config

--- src/main/java/by/radeflex/steamshop/configuration/SecurityConfig.java
+++ src/main/java/by/radeflex/steamshop/configuration/SecurityConfig.java
@@ -9,6 +9,7 @@ import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -18,6 +19,9 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @Configuration
 @EnableMethodSecurity
@@ -30,17 +34,33 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http, UserService userService) throws Exception {
         return http
                 .csrf(CsrfConfigurer::disable)
+                .cors(Customizer.withDefaults())
                 .authorizeHttpRequests(config ->
                         config
                                 .requestMatchers(HttpMethod.GET, "/products", "/products/*").permitAll()
                                 .requestMatchers("/login", "/register", "/logout", "/swagger-ui/**","/v3/api-docs/**").permitAll()
                                 .anyRequest().authenticated())
                 .authenticationProvider(authenticationProvider(userService))
-                .logout(config -> config.deleteCookies(jwtProperties.getCookieName()))
+                .logout(config -> config
+                        .deleteCookies(jwtProperties.getCookieName()))
                 .sessionManagement(manager -> manager.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class)
                 .build();
     }
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        config.addAllowedOrigin("http://localhost:3000");
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("*");
+
+        UrlBasedCorsConfigurationSource source =
+                new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config);
+        return source;
+    }
+
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
