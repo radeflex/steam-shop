cbdac284f834311cf2d7a4c128a95e032e802a49
Radeflex <maks.krav61@gmail.com>
2026-01-28 02:27:33 +0300

    minor

--- src/main/java/by/radeflex/steamshop/configuration/JwtProperties.java
+++ src/main/java/by/radeflex/steamshop/configuration/JwtProperties.java
@@ -3,11 +3,11 @@ package by.radeflex.steamshop.configuration;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
 
 @Setter
 @Getter
-@Configuration
+@Component
 @ConfigurationProperties(prefix = "jwt")
 public class JwtProperties {
     private String cookieName;
